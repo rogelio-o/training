cd52b4494c88e87c9eb732598e2dbcf7c18f4a9a
rogelio-o <yo@rogelioorts.com>
2019-04-02 21:23:40 +0200

    Adds problem CCI 1.8

---
+++ cci/1/1.8/problem-1-8.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+void solve(int m, int n, int **matrix) {
+    vector<pair<int,int>> zeros;
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] == 0) {
+                zeros.push_back(make_pair(i, j));
+            }
+        }
+    }
+
+    for(int zIndex = 0; zIndex < zeros.size(); zIndex++) {
+        pair<int, int> zeroPosition = zeros[zIndex];
+
+        for(int i = 0; i < m; i++) {
+            matrix[i][zeroPosition.second] = 0;
+        }
+
+        for(int j = 0; j < n; j++) {
+            matrix[zeroPosition.first][j] = 0;
+        }
+    }
+}
+
+int main()
+{
+    int numProblems;
+    cin >> numProblems;
+
+    for(int problemIndex = 1; problemIndex <= numProblems; problemIndex++) {
+        int m;
+        cin >> m;
+        int n;
+        cin >> n;
+
+        int **matrix;
+        matrix = new int*[m];
+        for(int i = 0; i < m; i++) {
+            matrix[i] = new int[n];
+            for(int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        solve(m, n, matrix);
+        cout << "Case #" << problemIndex << ":\n";
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+}
